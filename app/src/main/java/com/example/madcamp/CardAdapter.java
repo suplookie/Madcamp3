b27091f3580cb9e2ec364092dd0219b0cb85cf09
q <hsk0306@kaist.ac.kr>
2019-06-26 19:36:03 +0900

    second tab recycle layout

---
+++ app/src/main/java/com/example/madcamp/CardAdapter.java
@@ -0,0 +1,55 @@
+package com.example.madcamp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder> {
+
+    private ArrayList<String> mData = null ;
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView imageView1;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            imageView1 = itemView.findViewById(R.id.iv_photo);
+        }
+    }
+    CardAdapter(ArrayList<String> list) {
+        mData = list ;
+    }
+
+    @NonNull
+    @Override
+    public CardAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext() ;
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) ;
+
+        View view = inflater.inflate(R.layout.card, parent, false) ;
+        CardAdapter.ViewHolder vh = new CardAdapter.ViewHolder(view) ;
+
+
+
+        return vh ;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CardAdapter.ViewHolder holder, int position) {
+        String text = mData.get(position) ;
+        holder.imageView1.setText(text) ;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size() ;
+    }
+}
--- app/src/main/java/com/example/madcamp/SecondFragment.java
+++ app/src/main/java/com/example/madcamp/SecondFragment.java
@@ -4,8 +4,17 @@ package com.example.madcamp;
         import android.view.LayoutInflater;
         import android.view.View;
         import android.view.ViewGroup;
+        import android.widget.ImageView;
 
+        import androidx.annotation.NonNull;
         import androidx.fragment.app.Fragment;
+        import androidx.recyclerview.widget.LinearLayoutManager;
+        import androidx.recyclerview.widget.RecyclerView;
+
+        import com.google.android.material.floatingactionbutton.FloatingActionButton;
+        import com.google.android.material.snackbar.Snackbar;
+
+        import java.util.ArrayList;
 
 public class SecondFragment extends Fragment {
 
@@ -17,7 +26,40 @@ public class SecondFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_second, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        View main = inflater.inflate(R.layout.activity_main, container, false);
+
+        final ArrayList<String> list = new ArrayList<>();
+        for (int i=0; i<3; i++) {
+            list.add(String.format("TEXT %d", i)) ;
+        }
+
+        // 리사이클러뷰에 LinearLayoutManager 객체 지정.
+        RecyclerView recyclerView = view.findViewById(R.id.recycle2) ;
+
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity())) ;
+
+        // 리사이클러뷰에 SimpleTextAdapter 객체 지정.
+        final CardAdapter adapter = new CardAdapter(list) ;
+
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                list.add("JIII");
+                (adapter).notifyDataSetChanged();
+
+            }
+        });
+
+        recyclerView.setAdapter(adapter) ;
+
+
+        return view;
+
+
+        //return main;
     }
 }
