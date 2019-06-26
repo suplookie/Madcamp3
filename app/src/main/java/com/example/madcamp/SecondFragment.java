a7b196a4eda0754050ea98be0a5c0fcf6eca6bb4
q <hsk0306@kaist.ac.kr>
2019-06-26 13:45:23 +0900

    secondframgent demo

--- app/src/main/java/com/example/madcamp/SecondFragment.java
+++ app/src/main/java/com/example/madcamp/SecondFragment.java
@@ -1,8 +1,11 @@
 package com.example.madcamp;
 
-import android.os.Bundle;
+        import android.os.Bundle;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
+        import androidx.fragment.app.Fragment;
 
 public class SecondFragment extends Fragment {
 
@@ -10,6 +13,11 @@ public class SecondFragment extends Fragment {
         Bundle args = new Bundle();
         SecondFragment fragment = new SecondFragment();
         fragment.setArguments(args);
-        return new SecondFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_second, container, false);
     }
 }
