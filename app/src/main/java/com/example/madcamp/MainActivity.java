6640381032330b83d8826a3434463bbb67b67fc1
q <hsk0306@kaist.ac.kr>
2019-06-26 12:15:26 +0900

    how to swipe??

--- app/src/main/java/com/example/madcamp/MainActivity.java
+++ app/src/main/java/com/example/madcamp/MainActivity.java
@@ -12,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.madcamp.ui.main.SectionsPagerAdapter;
 
@@ -25,21 +26,60 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
-        //viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs= findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
 
-        tabs.addTab(tabs.newTab().setText("bbbb"));
+        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int pos = tab.getPosition();
+                changeView(pos);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
         FloatingActionButton fab = findViewById(R.id.fab);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action!!", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
 
             }
         });
+    }
+
+    private void changeView(int index) {
+        TextView textView1 = (TextView) findViewById(R.id.text1) ;
+        TextView textView2 = (TextView) findViewById(R.id.text2) ;
+        TextView textView3 = (TextView) findViewById(R.id.text3) ;
+        switch (index) {
+            case 0 :
+                textView1.setVisibility(View.VISIBLE) ;
+                textView2.setVisibility(View.INVISIBLE) ;
+                textView3.setVisibility(View.INVISIBLE) ;
+                break ;
+            case 1 :
+                textView1.setVisibility(View.INVISIBLE) ;
+                textView2.setVisibility(View.VISIBLE) ;
+                textView3.setVisibility(View.INVISIBLE) ;
+                break ;
+            case 2 :
+                textView1.setVisibility(View.INVISIBLE) ;
+                textView2.setVisibility(View.INVISIBLE) ;
+                textView3.setVisibility(View.VISIBLE) ;
+                break ;
+        }
     }
 }
